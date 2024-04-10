f948eb06fc6c41b272cf8e1564b4c6e4075cb942
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2024-04-10 14:04:26 +0300

    Embetter deploy

--- index.js
+++ index.js
@@ -7,38 +7,30 @@ const log = console.log
 
 app.use(koaBody())
 
+function isNonEmptyObject(b) {
+	return typeof b === 'object' && Object.keys(b).length !== 0
+}
+
 app.use(async ctx => {
 
-//	if (ctx.method === 'GET' && ctx.path === "/log")  {
 	let time = new Date().toISOString().replace('T', ' ').replace('Z', '')
 	if (ctx.query.message) {
 		const message = ctx.query.message
-		log(time, ctx.ip, ctx.path, message)
+		log(time, ctx.method, ctx.ip, ctx.path, message)
 		ctx.body = { result: "ok" }
 	} else {
 		let obj = ''
 
-		if (typeof ctx.query === 'object') {
-			console.log('!!! here')
-			if (Object.keys(ctx.query).length !== 0) {
-				console.log('!!! here2')
-				obj = JSON.stringify(ctx.query)
-			}
+		if (isNonEmptyObject(ctx.query)) {
+			obj = JSON.stringify(ctx.query)
 		}
 
-		if (typeof ctx.request.body === 'object') {
-			console.log('!!! here3')
-			if (Object.keys(ctx.request.body).length !== 0) {
-				console.log('!!! here4')
-				obj += ' ' + JSON.stringify(ctx.request.body)
-			}
+		if (isNonEmptyObject(ctx.request.body)) {
+			obj += ` ${JSON.stringify(ctx.request.body)}`
 		}
-		log(time, ctx.ip, ctx.path, obj.trim())
+		log(time, ctx.method, ctx.ip, ctx.path, obj.trim())
 		ctx.body = { error: "no message" }
 	}
-//	} else {
-//		ctx.body = { error: "404" }
-//	}
 })
 
 app.listen(8080)
